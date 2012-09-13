5edb1ac122f9788ecf9f90d61e05d58f9391c67f
Ugo Matrangolo <umatrangolo@gilt.com>
2012-09-13 09:14:55 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ToDo
+====
+
+Yet another TODO application.
+
+15-Sep-2012
+ugo.matrangolo@gmail.com
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '0d4d6c968d44fe45b8e741943d878042eb612a9a258be0469a07b10dc219feabe4fc2ca1f7409d945236b13a5bf266474f4d8a6939d8cd39a7d5a5c206d3ab83'
