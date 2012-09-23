1fb3fc08200e702196692837dec05fa62a776402
Ugo Matrangolo <umatrangolo@gilt.com>
2012-09-23 15:18:23 +0100

    Added days left indicator for each task

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,16 @@ class Task < ActiveRecord::Base
   validates :priority, numericality: { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5 }, presence: true
   validates :deadline, presence: true
   validates :status, presence: true, :inclusion => { :in => %w(Open InProgress Done), :message => "%{value} is not a valid state" }
+
+  def days_left
+    (deadline - DateTime.now).to_i
+  end
+
+  def passed?
+    (days_left < 0)
+  end
+
+  def expiring?
+    (days_left == 0)
+  end
 end
